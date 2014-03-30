81c19d54655525ba3699cc00a5551c41a3f1ad2f
Elias Nygren <elias.nygren@outlook.com>
2014-03-30 22:20:46 +0300

    routing bugfix

--- index.php
+++ index.php
@@ -21,13 +21,14 @@ require 'controllers/CustomerHoursController.php';
 require 'controllers/UsersController.php';
 require 'controllers/ReportController.php';
 
-if(preg_match('/^\//', $ru)) { LoginController::render(); }
-elseif(preg_match('/^\/hours\/add/',$ru)) { CustomerHoursController::create(); }
+
+if(preg_match('/^\/hours\/add/',$ru)) { CustomerHoursController::create(); }
 elseif(preg_match('/^\/hours/',$ru)) { CustomerHoursController::index(); }
 elseif(preg_match('/^\/users\/1/',$ru)) { Users::show(); }
 elseif(preg_match('/^\/users/',$ru)) { Users::index();  }
 elseif(preg_match('/^\/report/',$ru)) { Report::render();  }
 elseif(preg_match('/^\/test/',$ru)) { require 'connectionTest.php';  }
+elseif(preg_match('/^\//', $ru)) { LoginController::render(); }
 else{ echo "404";}
 //else { show_404(); }
 ?>
